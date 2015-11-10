e5f5d1b80658faf72d436885e0aab7a4c9038e2c
margotduek <margotduek@gmail.com>
2015-11-10 15:19:12 -0600

    h

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "xx",
-  password: "xx",
+  username: "margotduek",
+  password: "eemcdpsql1234",
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
